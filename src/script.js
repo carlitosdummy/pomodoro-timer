03b3fb3b41877355c364740872f06d06213c3cad
dummy <carlos256800@gmail.com>
2024-04-30 09:19:12 -0600

    Update Pomodoro Timer settings UI and functionality

--- src/script.js
+++ src/script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     //$ Variables
-
     //! Query selector shortcuts
     const $ = (selector) => document.querySelector(selector);
 
     //! Finish time audio
-    //todo: select user audio from the settings, for now, it's a default audio
+    //todo: select user audio from the settings, for now, it's a default audio (?)
     const finishTime = new Audio('../sounds/finish_time.mp3');
     finishTime.volume = 0.3;
     
@@ -22,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //! Default time for pomodoro, short break and large break
     //todo: save the settings in the local storage
-    //todo: chech if the user's input is a number and if it's not a number, show a message to the user
     let defaultTimePomodoro
     let defaultTimeShortBreak
     let defaultTimeLargeBreak
@@ -68,9 +66,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //? get the user's input 
     function getSetInputValue() {
-        defaultTimePomodoro = $('#pomodoro-input').value * 60;
-        defaultTimeShortBreak = $('#short-bk-input').value * 60;
-        defaultTimeLargeBreak = $('#large-bk-input').value * 60;
+        if ($('#pomodoro-input').value < 1) defaultTimePomodoro = 1 * 60
+        else if ($('#pomodoro-input').value > 180) defaultTimePomodoro = 180 * 60
+        else defaultTimePomodoro = $('#pomodoro-input').value * 60;
+        $('#pomodoro-input').value = defaultTimePomodoro / 60;
+
+        if ($('#short-bk-input').value < 1) defaultTimeShortBreak = 1 * 60
+        else if ($('#short-bk-input').value > 30) defaultTimeShortBreak = 30 * 60
+        else defaultTimeShortBreak = $('#short-bk-input').value * 60;
+        $('#short-bk-input').value = defaultTimeShortBreak / 60;
+
+        if ($('#large-bk-input').value < 1) defaultTimeLargeBreak = 1 * 60
+        else if ($('#large-bk-input').value > 60) defaultTimeLargeBreak = 60 * 60
+        else defaultTimeLargeBreak = $('#large-bk-input').value * 60;
+        $('#large-bk-input').value = defaultTimeLargeBreak / 60;
     }
 
     //? change and show the volume of the audio
@@ -132,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
         $timer.textContent = formattedTime;
     }
-
+    //todo: fix the sphere animation when the timer is paused It's skipping the some pixels
     //? Start, pause and reset the timer function
     $playButton.addEventListener('click', () => {
         if ($playButton.textContent === 'PLAY') {
@@ -141,8 +150,8 @@ document.addEventListener('DOMContentLoaded', () => {
             $outerSphere.classList.add('normal-blink');
             startTimer()
         }else if ($playButton.textContent === 'PAUSE') {
-            //todo: add a pause function
-            console.log('pause');
+            pauseTimer()
+            $outerSphere.classList.remove('normal-blink');
             $playButton.textContent = 'PLAY';
             $playButton.style.backgroundColor = '#45858896';
         } else {
@@ -160,6 +169,13 @@ document.addEventListener('DOMContentLoaded', () => {
         $sphere.style.animationPlayState = 'running';
         updateTimer()
         timerInterval = setInterval(updateTimer, 1000);
+    }
+
+    //? pauseTimer function
+    function pauseTimer() {
+        clearInterval(timerInterval);
+        $sphere.style.animationPlayState = 'paused';
+
     }
 
     //? stopTimer function
